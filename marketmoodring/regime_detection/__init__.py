b7d456e37d33d6bba13eecd273f39192f3cb1535
Yves D'hondt <36788808+yvesdhondt@users.noreply.github.com>
2023-07-14 17:55:09 -0700

    Update __init__.py

--- marketmoodring/regime_detection/__init__.py
+++ marketmoodring/regime_detection/__init__.py
@@ -1,3 +1,6 @@
+"""
+API reference documentation for the example regime detection models
+"""
 from marketmoodring.regime_detection.base import RegimeDetectionError
 from marketmoodring.regime_detection.hidden_markov import HiddenMarkovRegimeDetection
 from marketmoodring.regime_detection.wasserstein_kmeans import WassersteinKMeansRegimeDetection
